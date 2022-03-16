c70f2bc06ce66e5020f330ac93b06ccd91b1854b
Michał Staniszewski <stanisz96@gmail.com>
2022-03-16 16:39:56 +0100

    Add new parameters to User class

--- TwitterHelper.Api/Models/User.cs
+++ TwitterHelper.Api/Models/User.cs
@@ -10,5 +10,11 @@ namespace TwitterHelper.Api.Models
         public string id { get; set; }
         public string name { get; set; }
         public string username { get; set; }
+        public string created_at { get; set; }
+        public string description { get; set; }
+        public string location { get; set; }
+        public string public_metrics { get; set; }
+        public string verified { get; set; }
+
     }
 }
