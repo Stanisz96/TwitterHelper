44653f3fe98096c2343b4871e857dc08e5adb807
Michał Staniszewski <stanisz96@gmail.com>
2022-03-18 15:29:20 +0100

    Add new parameters to Tweet class and create ReferencedTweets class

---
+++ TwitterHelper.Api/Models/ReferencedTweets.cs
@@ -0,0 +1,9 @@
+﻿namespace TwitterHelper.Api.Models
+{
+    public class ReferencedTweets
+    {
+        public string Type { get; set; }
+        public string Id { get; set; }
+
+    }
+}
--- TwitterHelper.Api/Models/Tweet.cs
+++ TwitterHelper.Api/Models/Tweet.cs
@@ -7,10 +7,15 @@ namespace TwitterHelper.Api.Models
 {
     public class Tweet
     {
-        public string author_id { get; set; }
-        public string id { get; set; }
-        public string text { get; set; }
-        public string created_at { get; set; }
-        public string type { get; set; }
+        public string Author_id { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Created_at { get; set; }
+        public string Lang { get; set; }
+        public string Attachments { get; set; }
+        public string Entities { get; set; }
+        public PublicMetrics Public_metrics { get; set; }
+        public string Referenced_tweets { get; set; }
+
     }
 }
