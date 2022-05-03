4861fb3b10757dc9d97567f8459615a1ff9a377d
Michał Staniszewski <stanisz96@gmail.com>
2022-05-03 18:49:20 +0200

    Update DateTimeReference model

--- TwitterHelper.Web/Models/DateTimeReference.cs
+++ TwitterHelper.Web/Models/DateTimeReference.cs
@@ -1,23 +1,72 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace TwitterHelper.Web.Models
 {
     public class DateTimeReference
     {
         public int Id { get; set; }
-        public DateTime UsersLookupTime { get; set; }
-        public DateTime TweetsLookupTime { get; set; }
-        public DateTime TimelinesTime { get; set; }
-        public DateTime FilteredStreamTime { get; set; }
-        public DateTime FollowsTime { get; set; }
-
-        public DateTimeReference()
-        {
-            this.UsersLookupTime = DateTime.Now;
-            this.TweetsLookupTime = DateTime.Now;
-            this.TimelinesTime = DateTime.Now;
-            this.FilteredStreamTime = DateTime.Now;
-            this.FollowsTime = DateTime.Now;
+        public DateTime UsersLookupTime 
+        { 
+            get
+            {
+                return this.usersLookupTime.HasValue
+                    ? this.usersLookupTime.Value
+                    : DateTime.Now;
+            }
+            
+            set { this.usersLookupTime = value; } 
         }
+        public DateTime TweetsLookupTime
+        {
+            get
+            {
+                return this.tweetsLookupTime.HasValue
+                    ? this.tweetsLookupTime.Value
+                    : DateTime.Now;
+            }
+
+            set { this.tweetsLookupTime = value; }
+        }
+
+        public DateTime TimelinesTime
+        {
+            get
+            {
+                return this.timelinesTime.HasValue
+                    ? this.timelinesTime.Value
+                    : DateTime.Now;
+            }
+
+            set { this.timelinesTime = value; }
+        }
+        public DateTime FilteredStreamTime
+        {
+            get
+            {
+                return this.filteredStreamTime.HasValue
+                    ? this.filteredStreamTime.Value
+                    : DateTime.Now;
+            }
+
+            set { this.filteredStreamTime = value; }
+        }
+        public DateTime FollowsTime
+        {
+            get
+            {
+                return this.followsTime.HasValue
+                    ? this.followsTime.Value
+                    : DateTime.Now;
+            }
+
+            set { this.followsTime = value; }
+        }
+         
+        private DateTime? usersLookupTime = null; 
+        private DateTime? tweetsLookupTime = null; 
+        private DateTime? timelinesTime = null; 
+        private DateTime? filteredStreamTime = null; 
+        private DateTime? followsTime = null; 
     }
 }
