3d550329e282d9a339ee6ec27e506ea36da2d227
Michał Staniszewski <stanisz96@gmail.com>
2022-06-28 21:48:17 +0200

    change globals dir to dataV2

--- TwitterHelper.Api/Tools/Globals.cs
+++ TwitterHelper.Api/Tools/Globals.cs
@@ -2,8 +2,8 @@
 {
     public static class Globals
     {
-        public const string USERS_PATH = "C:\\Magisterka\\data\\users";
-        public const string DATA_PATH = "C:\\Magisterka\\data";
+        public const string USERS_PATH = "C:\\Magisterka\\dataV2\\users";
+        public const string DATA_PATH = "C:\\Magisterka\\dataV2";
         public const string ROOT_PATH = "C:\\Magisterka";
     }
 }
